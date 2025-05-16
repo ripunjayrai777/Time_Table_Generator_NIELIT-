cf26c0534ed6814e543142f234c9ebb4a0f8d3a1
ripunjayrai777 <ripunjayrai777@gmail.com>
2025-05-16 08:38:41 +0530

    ...

--- src/components/Print/index.jsx
+++ src/components/Print/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Print = () => {
-  return <div>Print page not Found new updates</div>;
-};
+  return <div>Print page not Found new update 212wwes</div>;
+}; 
 
 export default Print;
