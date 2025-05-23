55733e77513b91580928f2fd40483fa55c48628a
ripunjayrai777 <ripunjayrai777@gmail.com>
2025-05-23 16:42:43 +0530

    ...

--- src/components/Print/index.jsx
+++ src/components/Print/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Print = () => {
-  return <div>Print page not Foafdssdund new update fjfjnew update fhhf</div>;
+  return <div>Print page not Foafdssdund new update fjfjnew update fhhfff</div>;
 }; 
 
 export default Print;
